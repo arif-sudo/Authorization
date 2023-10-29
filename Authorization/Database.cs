f48af13f48e731d7a866f6e39a4aefb1c658c5ca
Arif <arif520gg@gmail.com>
2023-10-30 02:06:23 +0400

    MySql database setup

---
+++ Authorization/Database.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Authorization
+{
+    internal class Database
+    {
+        MySqlConnection connection = new MySqlConnection("server=localhost;username=root;database=c#test");
+        
+        public void openConnect()
+        {
+            if (connection.State == System.Data.ConnectionState.Closed)
+            {
+                connection.Open(); 
+            }
+        }
+
+        public void closeConnect()
+        {
+            if (connection.State == System.Data.ConnectionState.Open) 
+            { 
+                connection.Close();
+            }
+        }
+
+        public MySqlConnection getConnection() 
+        {
+            return connection;
+        }
+
+    }
+}
